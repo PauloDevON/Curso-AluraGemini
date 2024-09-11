c217f156a25fb746cd9a94a0bb4cc8e04994ef5b
PauloDevON <paulocesantanadev@gmail.com>
2024-09-10 21:44:14 -0300

    Adicionado mais codigos - aula 5 - AluraGemini

--- app.js
+++ app.js
@@ -2,19 +2,44 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
   
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    console.log(campoPesquisa)
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    
     // Inicializa uma string vazia para armazenar os resultados formatados
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada foi encontrado</p>"
+      return
+    } 
+      
     // Itera sobre cada dado no array 'dados' (não mostrado no código)
     for (let dado of dados) {
-      // Concatena HTML para cada dado, formatando o resultado
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        // Cria um novo elemento
       resultados += `
-        <div class="item-resultado">
-          <h2><a href="#">${dado.titulo}</a></h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href=${dado.link} target="_blank">Mais informações</a>
-        </div>
-      `;
+      <div class="item-resultado">
+        <h2><a href="#">${dado.titulo}</a></h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+      </div>
+    `;
+        
+      }      
+    }
+
+    if (!resultados){
+      resultados = "<p>Nada foi encontrado. Digite o nome de um deus</p>"
+
     }
   
     // Atribui o HTML gerado para a seção de resultados
--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let dados = [
     {
         titulo : "Zeus",
         descricao : "Zeus, na mitologia grega, era considerado o rei dos deuses e o governante do Monte Olimpo. Filho de Cronos e Reia, ele liderou a rebelião contra os Titãs, tornando-se a divindade suprema.",
-        link : "https://pt.wikipedia.org/wiki/Zeus"
+        link : "https://pt.wikipedia.org/wiki/Zeus",
+        tags : "raio trovao deus do olimpo"
     },
     {
-        titulo: "Poseidon",
-        descricao: "Poseidon, irmão de Zeus, era o deus dos mares, terremotos e cavalos na mitologia grega. Ele era frequentemente retratado com um tridente, sua arma característica.",
-        link: "https://pt.wikipedia.org/wiki.org/wiki/Poseidon"
+        titulo : "Poseidon",
+        descricao : "Poseidon, irmão de Zeus, era o deus dos mares, terremotos e cavalos na mitologia grega. Ele era frequentemente retratado com um tridente, sua arma característica.",
+        link : "https://pt.wikipedia.org/wiki.org/wiki/Poseidon",
+        tags : "mar agua oceano tridente"
     },
     {
-        titulo: "Hera",
-        descricao: "Hera era a rainha dos deuses na mitologia grega e esposa de Zeus. Deusa do casamento, da família e das mulheres, ela era conhecida por seu ciúme e vingança.",
-        link: "https://pt.wikipedia.org/wiki/Hera"
+        titulo : "Hera",
+        descricao : "Hera era a rainha dos deuses na mitologia grega e esposa de Zeus. Deusa do casamento, da família e das mulheres, ela era conhecida por seu ciúme e vingança.",
+        link : "https://pt.wikipedia.org/wiki/Hera",
+        tags : "ciumenta mulher do zeus moças"
     }
 ];
 
